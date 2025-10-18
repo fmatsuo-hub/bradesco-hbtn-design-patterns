68e154faa7d39f71671efb79f6006920cffb2fb3
Fabio <fabiolmatsuo@gmail.com>
2025-10-18 16:29:58 -0300

    hexagonal_architecture

---
+++ 0x05/hexagonal_architecture/src/ports/UserRepository.java
@@ -0,0 +1,9 @@
+package ports;
+import domain.User;
+import java.util.List;
+
+public interface UserRepository {
+    void save(User user);
+    User findById(String id);
+    List<User> findAll();
+}
